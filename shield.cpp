d3b293a7ab584da0d75c6c0956442e541c0fbf18
Dave <oblivion.davecave@gmail.com>
2017-08-06 16:15:39 -0300

    Now generating a integer vector as device list instead of string

--- shield.cpp
+++ shield.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <sstream>
+#include <vector>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
@@ -14,6 +16,7 @@
 // https://stackoverflow.com/questions/478898/how-to-execute-a-command-and-get-output-of-command-within-c-using-posix
 // https://stackoverflow.com/questions/2896600/how-to-replace-all-occurrences-of-a-character-in-string
 // https://unix.stackexchange.com/questions/17170/disable-keyboard-mouse-input-on-unix-under-x
+// https://ysonggit.github.io/coding/2014/12/16/split-a-string-using-c.html
 
 using namespace std;
 
@@ -33,15 +36,33 @@ string shell(const char* command) {
     return output;
 }
 
-string get_devices() {
-    string devlist = shell("xinput --list --id-only");
-    replace(devlist.begin(), devlist.end(), '\n', ' ');
+vector<string> split(const string &input, char delim) {
+    stringstream ss(input);
+    string item;
+    vector<string> tokens;
+    while(getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+vector<int> strvec2int(vector<string> input) {
+    vector<int> output;
+    for(int i=0; i < input.size(); i++) {
+        output.push_back(stoi(input[i]));
+    }
+    return output;
+}
+
+vector<int> get_devices() {
+    vector<string> devstr = split(shell("xinput --list --id-only"), '\n');
+    vector<int> devlist = strvec2int(devstr);
     return devlist;
 }
 
-bool change(string devlist, string prev_devlist) {
-    int prev_ndev = count(prev_devlist.begin(), prev_devlist.end(), ' ') + 1;
-    int ndev = count(devlist.begin(), devlist.end(), ' ') + 1;
+bool change(vector<int> devlist, vector<int> prev_devlist) {
+    int prev_ndev = prev_devlist.size();
+    int ndev = devlist.size();
     if(ndev != prev_ndev) return true;
     else return false;
 }
@@ -52,17 +73,17 @@ int main(int argc, char **argv) {
 
     while(true) {
 
-        string devlist = get_devices();
-        string prev_devlist = "";
+        vector<int> devlist = get_devices();
+        vector<int> prev_devlist;
         bool newdev = false;
 
         while(!newdev) {
-            string devlist = get_devices();
-            if(change(devlist, prev_devlist) && prev_devlist != "") {
+            devlist = get_devices();
+            if(change(devlist, prev_devlist) && prev_devlist.size() != 0) {
                 cout << "A change was detected" << endl;
             }
             this_thread::sleep_for(chrono::milliseconds(newdev_delay));
-            prev_devlist = devlist;
+            prev_devlist.assign(devlist.begin(), devlist.end());
         }
     }
 
@@ -70,7 +91,7 @@ int main(int argc, char **argv) {
     // struct input_event ev;
 
     // while (true) {
-        
+
     //     read(fd, &ev, sizeof(struct input_event));
     //     if(ev.type == 1)
     //         cout << "key " << ev.code << " state " << ev.value << endl;
