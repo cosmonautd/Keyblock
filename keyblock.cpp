4634e4abf745a7fecc453bd788d27b3a0e83b954
dave <david.borges@protonmail.com>
2017-09-08 17:45:31 +0000

    Merge

--- keyblock.cpp
+++ keyblock.cpp
@@ -150,11 +150,7 @@ bool attack_similarity_analysis(vector<double> keystroke_press) {
     The fixed values are computed from empirical data.
 */
 bool max_allowed_human_deviation(vector<double> keystroke_press) {
-<<<<<<< HEAD
     double tau_d = 31.64*1000000 - 3*9.63*1000000;
-=======
-    double tau_d = 31.64*1000000 - 2.5*9.63*1000000;
->>>>>>> b09d7f90c1cdd85328b3ea5cf9d6052f8f4073ac
     double tau_s = 0.1*9.63*1000000;
     double delays[keystroke_press.size() - 1];
     if(keystroke_press.size() > 1) {
